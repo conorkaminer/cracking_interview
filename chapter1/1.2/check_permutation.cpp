28c56da3cd14b765d38d59efa6e6baee9d248e26
Conor Kaminer <conorkaminer@gmail.com>
2021-01-19 05:15:19 +0000

    Some changes not sure aboutSome changes not sure about

--- chapter1/1.2/check_permutation.cpp
+++ chapter1/1.2/check_permutation.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
-int main (int argc, char **argv) {
+bool isPermutation(string a, string b) {
+    if (a.length() != b.length()) {
+        return false;
+    } else {
+        
+    }
+}
 
+int main (int argc, char **argv) {
+    vector<string> args;
+
+    if (argc > 1) {
+        args.assign(argv + 1, argv + argc);
+    }
+
+    if (isPermutation(args[0], args[1]) {
+        cout << "Is Permutation" ;
+    } else {
+        cout << "Not a Permutation" ;
+    }
 }
